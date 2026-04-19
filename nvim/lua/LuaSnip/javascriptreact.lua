01131ac5674e3dd13e731f023b2b3a3590c7ca1c
Ruthless Calm <ruthlesscalm@proton.me>
2026-04-19 21:47:13 +0530

    Add react snippets

--- nvim/lua/LuaSnip/javascriptreact.lua
+++ nvim/lua/LuaSnip/javascriptreact.lua
@@ -1,1 +1,39 @@
-return require("LuaSnip.javascript")
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local t = ls.text_node
+local rep = require("luasnip.extras").rep
+
+-- Import your base JS snippets (arfn, harfn, etc.)
+local snippets = require("LuaSnip.javascript")
+
+-- Add JSX-specific snippets
+-- <Component />
+table.insert(snippets, s("com", {
+  t("<"), i(1, "Component"), t(" "), i(2), t(" />"),
+}))
+
+-- <tag></tag> (Mirrored)
+table.insert(snippets, s("tag", {
+  t("<"), i(1, "div"), t(">"),
+  i(2),
+  t("</"), rep(1), t(">"),
+}))
+
+-- <Component> ... </Component>
+table.insert(snippets, s("comb", {
+  t("<"), i(1, "Component"), t(">"),
+  t({ "", "  " }),
+  i(2),
+  t({ "", "</" }), rep(1), t(">"),
+}))
+
+-- Fragment <> </>
+table.insert(snippets, s("frag", {
+  t("<>"),
+  t({ "", "  " }),
+  i(1),
+  t({ "", "</>" }),
+}))
+
+return snippets
