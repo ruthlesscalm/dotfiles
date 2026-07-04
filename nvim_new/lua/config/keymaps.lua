850db3a18691f0e5f99797a3e791ae29028a662a
Ruthless Calm <ruthlesscalm@proton.me>
2026-07-04 21:39:19 +0530

    Set Space as mapleader
    Configure tab and indentation defaults
    
    Enable relative line numbers and cursorline
    
    Enable gui colors
    
    Add netrw explorer keymap
    
    Add keymaps to save and source the current buffer

---
+++ nvim_new/lua/config/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>e", "<cmd>Ex<CR>") -- Netrw Explorer
+vim.keymap.set("n", "<leader>sc", "<cmd>source %<CR>") -- source current buffer
+vim.keymap.set("n", "<C-s>", "<cmd>w<CR>") -- save buffer
--- nvim_new/lua/config/options.lua
+++ nvim_new/lua/config/options.lua
@@ -1,3 +1,10 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
+
+vim.opt.termguicolors = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.smartindent = true
