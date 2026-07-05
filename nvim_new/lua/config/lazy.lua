c1de46c34b87c0e2a8e0f8e89b5aaeafce4edfd1
Ruthless Calm <ruthlesscalm@proton.me>
2026-07-05 09:03:28 +0530

    install catppuccin colorscheme and autopairs plugin

--- nvim_new/lua/config/lazy.lua
+++ nvim_new/lua/config/lazy.lua
@@ -27,9 +27,13 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
+  change_detection = {
+	  enabled = false,
+	  notify = false,
+  },
+ checker = { enabled = false },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  -- install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
 })
