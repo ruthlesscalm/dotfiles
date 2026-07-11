b976a5099f520916dae11cdd9704518bc2ada0db
Ruthless Calm <ruthlesscalm@proton.me>
2026-07-11 08:47:41 +0530

    Customize telescope

--- nvim_new/lua/plugins/telescope.lua
+++ nvim_new/lua/plugins/telescope.lua
@@ -1,8 +1,32 @@
+local builtin = require("telescope.builtin")
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
+	"nvim-telescope/telescope.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		-- optional but recommended
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+	keys = {
+		{
+			"<leader>ff",
+			builtin.find_files,
+			desc = "Find Files",
+		},
+		{
+			"<leader>fg",
+			builtin.live_grep,
+			desc = "Live Grep",
+		},
+		{
+			"<leader>fb",
+			builtin.buffers,
+			desc = "Buffers",
+		},
+		{
+			"<leader>fh",
+			builtin.help_tags,
+			desc = "help tags",
+		},
+	},
 }
