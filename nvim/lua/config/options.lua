c9c38aba5958ab0fd2ee2131ba15eae153a89030
Ruthless Calm <ruthlesscalm@proton.me>
2026-07-25 17:38:03 +0530

    change default tabsize and use the default harpoon menu instead of telescope ui

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,8 +4,9 @@ vim.opt.cursorline = true
 
 vim.opt.termguicolors = true
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
