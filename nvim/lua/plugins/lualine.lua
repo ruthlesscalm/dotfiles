1f56539bf39d789d457c417ac628ec35bae7ce00
Ruthless Calm <ruthlesscalm@proton.me>
2026-07-29 13:08:27 +0530

    remove fileformat and encoding from lua line

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,12 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-      theme = "nightfox"
-    }
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			theme = "nightfox",
+		},
+		sections = {
+			lualine_x = { "filetype" },
+		},
+	},
 }
