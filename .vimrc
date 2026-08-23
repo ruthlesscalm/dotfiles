8d4d7840568b050d8c9df38c0bbeb055ed5ae4b1
Ruthless Calm <ruthlesscalm@proton.me>
2026-08-23 13:09:58 +0530

    Change shortcuts in both vim and neovim

--- .vimrc
+++ .vimrc
@@ -12,27 +12,44 @@ set incsearch hlsearch ignorecase smartcase
 set path+=**
 set wildmenu
 
+set mouse=a
+
 let mapleader = " "
+
+" Find file
 nnoremap <leader>ff :find 
-nnoremap <leader>e :Ex<CR> 
-nnoremap <M-q> :q<CR>
+
+" File Explorer
+nnoremap <leader>e :Ex<CR>
+
+" Save
 nnoremap <C-s> :w<CR>
+inoremap <C-s> <Esc>:w<CR>
+
+" Quit
+nnoremap <C-q> :q<CR>
+
+" System clipboard register
+nnoremap <leader>c "+
+xnoremap <leader>c "+
+
+" Delete current buffer without closing window
+nnoremap <leader>bd :bp \| bd #<CR>
+
+" Next buffer
+nnoremap <leader><Tab> :bnext<CR>
+
+" Visual indentation
+vnoremap <Tab> >gv
+vnoremap <S-Tab> <gv
+
+" Terminal mode
+if exists('##terminal')
+    tnoremap <C-Esc> <C-\><C-n>
+endif
+
+" New tab
+nnoremap <leader>tn :tabnew<CR>
 
 colorscheme habamax
 set termguicolors
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,21 +3,18 @@ vim.keymap.set("n", "<leader>e", "<cmd>Ex<CR>", {desc = "Open File Explorer"}) -
 vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", {desc = "Save current buffer"}) 
 vim.keymap.set("i", "<C-s>", "<esc><cmd>w<CR>", {desc = "Save current buffer"}) 
 
-vim.keymap.set("n", "<M-q>", "<cmd>q<CR>", {desc = "Quit Nvim"})
+vim.keymap.set("n", "<C-q>", "<cmd>q<CR>", {desc = "Quit Window"})
 
 vim.keymap.set({"n", "x"}, "<leader>c", '"+', {desc = "System clipboard register (+)"})
 
-vim.keymap.set("n", "<M-b>", "<cmd>bdelete<CR>", {desc = "Delete Current buffer"})
-vim.keymap.set( "n", "<M-Tab>", "<cmd>bnext<CR>", { desc = "Next Buffer" })
-vim.keymap.set( "n", "<M-S-Tab>", "<cmd>bprev<CR>", { desc = "Previous Buffer" })
+vim.keymap.set("n", "<leader>bd", "<cmd>bp | bd #<CR>", {desc = "Delete Current buffer"})
+vim.keymap.set( "n", "<leader><Tab>", "<cmd>bnext<CR>", { desc = "Next Buffer" })
 
 vim.keymap.set("v", "<Tab>", ">gv", { desc = "Indent selection" })
 vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Un-indent selection" })
 
 vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, {desc = "Open signature_help"})
 
-vim.keymap.set('t', '<M-Esc>', [[<C-\><C-n>]], { desc = 'Exit terminal mode' })
-
-vim.keymap.set('n', '<leader>tm', "<cmd>term<CR>" , { desc = 'Open Terminal' })
+vim.keymap.set('t', '<C-Esc>', [[<C-\><C-n>]], { desc = 'Exit terminal mode' })
 
 vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>", {desc = "Open new tab"})
