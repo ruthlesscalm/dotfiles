5acbddd2ca2e4893af4c50f3ee3ad6c827903906
Ruthless Calm <ruthlesscalm@proton.me>
2026-08-27 19:39:23 +0530

    Add gnome keyring daemon and also remove unued colorschemes in nvim

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -23,39 +23,6 @@ return {
 		"AlexvZyl/nordic.nvim",
 	},
 	{
-		"olimorris/onedarkpro.nvim",
-	},
-	{
-		"rebelot/kanagawa.nvim",
-	},
-	{
-		"sainnhe/gruvbox-material",
-	},
-	{
-		"sainnhe/sonokai",
-	},
-	{
-		"sainnhe/edge",
-	},
-	{
-		"alexvzyl/nordic.nvim",
-	},
-	{
-		"tiagovla/tokyodark.nvim",
-	},
-	{
-		"folke/tokyonight.nvim",
-	},
-	{
-		"ribru17/bamboo.nvim",
-	},
-	{
-		"mofiqul/dracula.nvim",
-	},
-	{
-		"NTBBloodbath/sweetie.nvim",
-	},
-	{
 		"ellisonleao/gruvbox.nvim",
 	},
 	{
